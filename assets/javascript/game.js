ddac83777b7694405fd28f7f8c70a0699ea38cde
pruder <alexthebaker777@yahoo.com>
2016-11-13 23:22:58 -0600

    Homework submission incomplete mathematical logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+//win and loss counters begin//
+var wins = 0;
+var losses = 0;
+//gemstone buttons randomisers//
+var red gem= Math.floor((Math.random()* 13));
+var blue gem= Math.floor((Math.random()* 13));
+var yellow gem= Math.floor((Math.random()* 13));
+var green gem= Math.floor((Math.random()* 13));
+//game reset//
+var reset = function(){
+//gems reset//
+	red gem= Math.floor((Math.random()* 13));
+    blue gem= Math.floor((Math.random()* 13));
+    yellow gem= Math.floor((Math.random()* 13));
+    green gem= Math.floor((Math.random()* 13));
+}
+//click functions for gems
+	$('#red gem').click(function(){
+		;
+	})
+	$('#blue gem').click(function(){
+		;
+	})
+	$('#yellow gem').click(function(){
+		;
+	})
+	$('#green gem').click(function(){
+		;
+	});
